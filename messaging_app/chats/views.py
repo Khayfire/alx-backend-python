de64d4b1f7cb2bc7cda33587f9e89b3460aa318e
khayfire <kayode354@gmail.com>
2025-09-23 07:08:50 +0100

    chats

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, filters
+# chats/views.py
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -6,18 +8,26 @@ from .serializers import ConversationSerializer, MessageSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["participants__username"]
 
-    # Add filtering & searching
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ["title", "participants__username"]
-    ordering_fields = ["id", "title"]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["sender__username", "conversation__id"]
 
-    # Add filtering & searching
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ["content", "sender__username"]
-    ordering_fields = ["timestamp"]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
